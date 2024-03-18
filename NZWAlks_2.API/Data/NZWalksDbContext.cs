b1c749a475b431791a968333c5f16c22647b2d27
baraiyamaulik77@gmail.com <baraiyamaulik77@gmail.com>
2024-03-18 15:39:20 +0530

    End of Session 2

---
+++ NZWAlks_2.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NZWAlks_2.API.Models.Domain;
+
+namespace NZWAlks_2.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {    
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ NZWAlks_2.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWAlks_2.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; } 
+        public string Code { get; set; }                
+    }
+}
